c5510c9027e45c8c0516492ed90f3d53aaf4e73e
Tiger Chan <tigerhk@gmail.com>
2020-04-19 01:03:07 +0800

    customizable file to .example

--- certbot/renew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-certbot renew
-
-bash -c "cat /etc/letsencrypt/live/demo.scalinglaravel.com/fullchain.pem /etc/letsencrypt/live/demo.scalinglaravel.com/privkey.pem > /etc/ssl/demo.scalinglaravel.com/demo.scalinglaravel.com.pem"
-
-source /root/haproxy/reload.sh
--- haproxy/reload.sh
+++ haproxy/reload.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-haproxy -f /etc/haproxy/haproxy.cfg -p /run/haproxy.pid -sf $(cat /run/haproxy.pid) -x /var/run/haproxy.sock
+cp /root/cluster-starter-kit/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg
+systemctl reload haproxy
+#haproxy -f /etc/haproxy/haproxy.cfg -p /run/haproxy.pid -sf $(cat /run/haproxy.pid) -x /var/run/haproxy.sock
