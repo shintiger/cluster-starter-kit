de720ef9bc175833dc930acdd160139bde02c7fd
Tiger Chan <tigerhk@gmail.com>
2020-09-13 13:45:40 +0800

    [ADD] dataplaneapi related

---
+++ haproxy/dataplaneapi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dataplaneapi --host 0.0.0.0 --port 5555 --userlist dataplaneapi
--- haproxy/install.sh
+++ haproxy/install.sh
@@ -5,4 +5,17 @@ sudo add-apt-repository ppa:vbernat/haproxy-2.0
 sudo apt update
 sudo apt install -y haproxy
 
+mkdir dataplaneapi
+pushd dataplaneapi
+wget https://github.com/haproxytech/dataplaneapi/releases/download/v2.1.0/dataplaneapi_2.1.0_Linux_x86_64.tar.gz
+tar -zxfv dataplaneapi_2.1.0_Linux_x86_64.tar.gz
+pushd build
+chmod +x dataplaneapi
+sudo cp dataplaneapi /usr/local/bin/ 
+popd
+popd
+rm -rf dataplaneapi
+
+sudo cp dataplaneapi.service /etc/systemd/system/
+
 # sudo nano /etc/haproxy/haproxy.cfg
