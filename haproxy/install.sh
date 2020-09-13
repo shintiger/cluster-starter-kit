7cbbc1cd42c777718bd359dad833b2147359a4e0
Tiger Chan <tigerhk@gmail.com>
2020-09-13 13:52:38 +0800

    [FIX] wrong options

--- haproxy/install.sh
+++ haproxy/install.sh
@@ -8,7 +8,7 @@ sudo apt install -y haproxy
 mkdir dataplaneapi
 pushd dataplaneapi
 wget https://github.com/haproxytech/dataplaneapi/releases/download/v2.1.0/dataplaneapi_2.1.0_Linux_x86_64.tar.gz
-tar -zxfv dataplaneapi_2.1.0_Linux_x86_64.tar.gz
+tar -zxvf dataplaneapi_2.1.0_Linux_x86_64.tar.gz
 pushd build
 chmod +x dataplaneapi
 sudo cp dataplaneapi /usr/local/bin/ 
