d5ecc654ce22d53195702c98675210c7e4f353ed
Tiger Chan <tigerhk@gmail.com>
2020-09-25 00:24:03 +0800

    no message

--- registry/02_run.sh
+++ registry/02_run.sh
@@ -5,7 +5,9 @@ docker run -d \
   --restart=always \
   --name registry \
   -v "$(pwd)"/auth:/auth \
+  -v "$(pwd)"/registry:/var/lib/registry \
   -e "REGISTRY_AUTH=htpasswd" \
   -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
   -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+  -e REGISTRY_HTTP_RELATIVEURLS=true \
   registry:2
