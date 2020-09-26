c522569591d3b0927cc2cc55d30be44dd3685e92
Tiger Chan <tigerhk@gmail.com>
2020-09-26 15:52:13 +0800

    -

--- haproxy/reload.sh
+++ haproxy/reload.sh
@@ -2,4 +2,5 @@
 
 cp /root/cluster-starter-kit/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg
 systemctl reload haproxy
+echo "show servers state" | socat stdio /var/run/haproxy.sock > /usr/local/haproxy/haproxy.state
 #haproxy -f /etc/haproxy/haproxy.cfg -p /run/haproxy.pid -sf $(cat /run/haproxy.pid) -x /var/run/haproxy.sock
