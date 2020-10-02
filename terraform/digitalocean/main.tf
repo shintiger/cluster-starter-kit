0d0cfad721ad7ccd3d66f66f514a9f7358c0ab75
Tiger Chan <tigerhk@gmail.com>
2020-10-03 02:16:28 +0800

    -

--- terraform/digitalocean/main.tf
+++ terraform/digitalocean/main.tf
@@ -12,6 +12,7 @@ variable "swarm" {
   type = object({
     token   = string
     address = string
+    gelf_address = string
   })
 }
 
@@ -51,7 +52,7 @@ resource "digitalocean_droplet" "lolotiger" {
   private_networking = true
   resize_disk        = false
   tags               = var.cluster.tags
-  user_data          = templatefile("./cloud-config-frontend.yml", { swarm_token = var.swarm.token, swarm_address = var.swarm.address, role = "frontend" })
+  user_data          = templatefile("./cloud-config-gelf.yml", { swarm_token = var.swarm.token, swarm_address = var.swarm.address, role = "frontend" })
 }
 
 resource "digitalocean_droplet" "lolotiger-logging" {
@@ -65,5 +66,19 @@ resource "digitalocean_droplet" "lolotiger-logging" {
   private_networking = true
   resize_disk        = false
   tags               = ["logging"]
-  user_data          = templatefile("./cloud-config.yml", { swarm_token = var.swarm.token, swarm_address = var.swarm.address, role = "logging" })
+  user_data          = templatefile("./cloud-config.yml", { swarm_token = var.swarm.token, swarm_address = var.swarm.address, role = "logging", gelf_address = var.swarm.gelf_address })
+}
+
+resource "digitalocean_droplet" "lolotiger-controller" {
+  count              = 1
+  image              = "docker-20-04"
+  name               = "lolotiger-controller-${(count.index) % 100}"
+  region             = "sgp1"
+  size               = "s-1vcpu-2gb"
+  ssh_keys           = var.do_keys
+  monitoring         = true
+  private_networking = true
+  resize_disk        = false
+  tags               = ["controller"]
+  user_data          = templatefile("./cloud-config-gelf.yml", { swarm_token = var.swarm.token, swarm_address = var.swarm.address, role = "logging", gelf_address = var.swarm.gelf_address })
 }
